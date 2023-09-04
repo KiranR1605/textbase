177456e49c0ad3fcd2d9d4d8a01fe2b28eead5ee
KiranR1605 <kiranr1605@gmail.com>
2023-09-04 11:54:12 +0530

    financial advisor

--- examples/openai-bot/main.py
+++ examples/openai-bot/main.py
@@ -1,25 +1,55 @@
 from textbase import bot, Message
 from textbase.models import OpenAI
 from typing import List
+import openai
+from scipy.spatial.distance import cosine
+import matplotlib.pyplot as plt
+from openai.embeddings_utils import get_embedding
+from openai.embeddings_utils import cosine_similarity
+import pandas as pd
 
 # Load your OpenAI API key
-OpenAI.api_key = ""
-
-# Prompt for GPT-3.5 Turbo
-SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like.
-The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a
-pleasant chat!
-"""
+openai.api_key = "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"
+OpenAI.api_key = "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"
 
 @bot()
 def on_message(message_history: List[Message], state: dict = None):
 
-    # Generate GPT-3.5 Turbo response
+    episode = pd.read_csv("examples\openai-bot\question_embeddings.csv")
+    
+    question = message_history[-1]['content'][-1]['value']
+    question_vector = get_embedding(question, engine='text-embedding-ada-002')
+
+    episode["similarities"] = episode['embedding'].apply(lambda x: cosine_similarity(x, question_vector))
+    episode = episode.sort_values("similarities", ascending=False).head(4)
+
+    episode.to_csv("sorted.csv")
+
+    context = []
+    for i, row in episode.iterrows():
+        context.append(row['context'])
+
+    SYSTEM_PROMPT = f"""Answer the following question using only the context below. If you don't know the answer for certain, say I don't know.
+
+Context:
+{context[1][:10000]}
+
+Q: {question}
+A:"""
+
+#     SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like.
+# The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a
+# pleasant chat!
+# """
+
     bot_response = OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
-        message_history=message_history, # Assuming history is the list of user messages
         model="gpt-3.5-turbo",
+        system_prompt=SYSTEM_PROMPT,
+        message_history=message_history
     )
+
+    print(message_history)
+    print(Message)
 
     response = {
         "data": {
